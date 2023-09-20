67ec8513035366bc90435dfe54fc019969d9a39c
Georgina <georginatomass@gmail.com>
2023-09-20 14:16:36 +0200

    landing + styles

--- front/src/pages/LandingPage.tsx
+++ front/src/pages/LandingPage.tsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom"
 import Logo from "../assets/img/logo.png"
-import { Container } from "react-bootstrap"
+import  style from '../App.module.css'
 import  styles from '../styles/pages/LandingPage.module.css'
 
 export const LandingPage = () => {
   return (
     <>
-    <Container className={styles.container}>
-    <h1>Benvingut a </h1>
-      <Link to='/'> <img src={Logo} alt="" /></Link>
-    </Container>
+    <section className={style.container}>
+      <div className={style.content}>
+        <img src="../src/assets/img/veg_ani.gif" className={styles.img} />
+      <Link to='/home'> <h1>Gaudeix  i menja sa</h1></Link>
+      <img src={Logo} alt="" />
+      </div>
+    </section>
     </>
   )
 }
